6553753007b99b5ee1869d1ac2d3f0b9e7c4c120
gongling <gongling@ifeng.com>
2016-11-11 15:01:08 +0800

    加分类编辑页

--- myapplication/src/main/java/com/example/gongling/myapplication/ormsql/ProductManageActivity.java
+++ myapplication/src/main/java/com/example/gongling/myapplication/ormsql/ProductManageActivity.java
@@ -2,38 +2,94 @@ package com.example.gongling.myapplication.ormsql;
 
 import android.content.Context;
 import android.content.Intent;
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.view.Menu;
-import android.view.MenuItem;
-import com.example.gongling.myapplication.*;
+import android.support.design.widget.FloatingActionButton;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.GridLayoutManager;
+import android.support.v7.widget.RecyclerView;
+import android.view.View;
+import android.widget.Toast;
+import com.example.gongling.myapplication.R;
+import com.example.gongling.myapplication.ormsql.dao.ProductCategoryDao;
+import com.example.gongling.myapplication.ormsql.dao.ProductDao;
+import com.example.gongling.myapplication.ormsql.db.Product;
+import com.example.gongling.myapplication.ormsql.db.ProductCategory;
 
-public class ProductManageActivity extends AppCompatActivity {
+import java.util.List;
+
+public class ProductManageActivity extends AppCompatActivity implements ProMagAdapter.MyItemClickListener {
+
+    RecyclerView catGridList;
+    ProductCategoryDao productCategoryDao;
+    ProductDao productDao;
+    List<ProductCategory> categories;
+    ProMagAdapter adpter;
+    boolean isShowFragment = false;
 
     public static void intentTo(Context context) {
         context.startActivity(new Intent(context, ProductManageActivity.class));
     }
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_product_manage);
-        setTitle("分类管理");
+        catGridList = (RecyclerView) findViewById(R.id.cat_list);
+        catGridList.setLayoutManager(new GridLayoutManager(this, 3));
+        productCategoryDao = new ProductCategoryDao(this);
+        productDao = new ProductDao(this);
+        categories = productCategoryDao.listProductCat();
+        adpter = new ProMagAdapter(this, categories);
+        adpter.setOnItemClickListener(this);
+        catGridList.setAdapter(adpter);
+
+        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
+        fab.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                isShowFragment = false;
+                getFragmentManager().popBackStack();
+            }
+        });
+    }
+
+
+    @Override
+    public void onItemClick(View view, int postion) {
+        if (postion == categories.size()) {
+            Toast.makeText(this, "more", Toast.LENGTH_SHORT).show();
+            ProductCategory category = new ProductCategory(postion, "洗衣粉类" + postion, "各种");
+            productCategoryDao.addProductCat(category);
+            for (int j = 0; j < 10; j++) {
+                Product product = new Product(111l, "abc" + j, 12.5, "");
+                product.setCatid(category.getId());
+                productDao.addProduct(product);
+            }
+            categories = productCategoryDao.listProductCat();
+            adpter = new ProMagAdapter(this, categories);
+            catGridList.setAdapter(adpter);
+        } else {
+            Toast.makeText(this, postion + "", Toast.LENGTH_SHORT).show();
+            ProductManageFragment f = new ProductManageFragment();
+            Bundle b = new Bundle();
+            b.putInt("cid", categories.get(postion).getId());
+            b.putInt("position", postion);
+            f.setArguments(b);
+            getFragmentManager().beginTransaction()
+                    .replace(R.id.manager_container, f)
+                    .addToBackStack(null)
+                    .commit();
+            isShowFragment = true;
+        }
     }
 
     @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
-       // getMenuInflater().inflate(R.menu.menu_product_manager, menu);
-        super.onCreateOptionsMenu(menu);
-        // menu.add(Menu.NONE, Menu.FIRST+1, 0, "设置");
-        return true;
-    }
-
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-
-        int id = item.getItemId();
-
-        return super.onOptionsItemSelected(item);
+    public void onBackPressed() {
+        if (isShowFragment) {
+            isShowFragment = false;
+            getFragmentManager().popBackStack();
+        } else {
+            super.onBackPressed();
+        }
     }
 }
---
+++ myapplication/src/main/java/com/example/gongling/myapplication/ormsql/ProductManageFragment.java
@@ -0,0 +1,57 @@
+package com.example.gongling.myapplication.ormsql;
+
+import android.app.Fragment;
+import android.os.Bundle;
+import android.support.v7.widget.GridLayoutManager;
+import android.support.v7.widget.RecyclerView;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.Toast;
+import com.example.gongling.myapplication.R;
+import com.example.gongling.myapplication.ormsql.dao.ProductDao;
+import com.example.gongling.myapplication.ormsql.db.Product;
+
+import java.util.List;
+
+public class ProductManageFragment extends Fragment implements ProAdapter.MyItemClickListener{
+    private static final String TAG = "TestFragment";
+
+    int position;
+    int curCatId;
+    RecyclerView prolistView;
+    List<Product> products;
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        Bundle b = this.getArguments();
+        position = b.getInt("position");
+        curCatId = b.getInt("cid");
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+        View view = inflater.inflate(R.layout.fragment_product, null);
+        return view;
+    }
+
+    @Override
+    public void onViewCreated(View view, Bundle savedInstanceState) {
+
+        prolistView = (RecyclerView) view.findViewById(R.id.pro_list);
+        GridLayoutManager gridLayoutManager = new GridLayoutManager(getActivity(), 1);
+        prolistView.setLayoutManager(gridLayoutManager);
+        ProductDao productDao = new ProductDao(getActivity());
+        products = productDao.listProductByCat(curCatId);
+        ProAdapter adpter=new ProAdapter(getActivity(), products);
+        adpter.setOnItemClickListener(this);
+        prolistView.setAdapter(adpter);
+
+    }
+
+    public void onItemClick(View view, int postion) {
+
+        Toast.makeText(getActivity(), "postion:"+postion, Toast.LENGTH_SHORT).show();
+    }
+}
